0ee12b1c8dc6768d9cba6a67474d09c6f5569a29
andyltkeller <andy@theoremreach.com>
2020-10-01 11:55:34 -0700

    Create separate TS frameworks for iOS and simulator

---
+++ simulator/TheoremReachSDK.framework/Headers/TRSurveyView.h
@@ -0,0 +1,41 @@
+//
+//  TRSurveyView.h
+//
+//  Created by TheoremReach on 11/22/15.
+//  Copyright (c) 2018 TheoremReach. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <WebKit/WebKit.h>
+
+@class TRSurveyView;
+
+@protocol TRSurveyViewDelegate<NSObject>
+
+- (void)TRSurveyView:(TRSurveyView *)trSurveyView donePushed:(id)sender;
+
+@end
+
+@interface TRSurveyView : WKWebView <WKNavigationDelegate>
+
+@property NSString *baseUrl;
+
+@property (strong, nonatomic) IBOutlet UIView *containerView;
+@property (strong, nonatomic) IBOutlet UIToolbar *navBar;
+@property (strong, nonatomic) IBOutlet UIBarButtonItem *doneButton;
+@property (strong, nonatomic) IBOutlet UIBarButtonItem *rewardsCenterButton;
+@property (strong, nonatomic) IBOutlet UIBarButtonItem *flex;
+@property (strong, nonatomic) IBOutlet UIBarButtonItem *toolbarTitle;
+@property (strong, nonatomic) IBOutlet UIBarButtonItem *refreshButton;
+@property (strong, nonatomic) IBOutlet UILabel *toolbarTitleLabel;
+@property (strong, nonatomic) IBOutlet WKWebView *iframeView;
+@property (strong, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
+@property (strong, nonatomic) IBOutlet UIView *overlay;
+
+@property (strong, nonatomic) id<TRSurveyViewDelegate> delegate;
+
+- (UIToolbar *)buildNavBarWithY:(float)y width:(float)width height:(float)height;
+- (void)setupNavBarButtons;
+- (void)resizeRewardsCenterView;
+
+@end
---
+++ simulator/TheoremReachSDK.framework/Headers/TheoremReachSDK.h
@@ -0,0 +1,23 @@
+//
+//  TheoremReachSDK.h
+//  TheoremReachSDK
+//
+//  Created by TheoremReach on 12/22/15.
+//  Copyright © 2018 TheoremReach. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TheoremReachSDK.
+FOUNDATION_EXPORT double TheoremReachSDKVersionNumber;
+
+//! Project version string for TheoremReachSDK.
+FOUNDATION_EXPORT const unsigned char TheoremReachSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TheoremReachSDK/PublicHeader.h>
+
+#import <TheoremReachSDK/TheoremReach.h>
+#import <TheoremReachSDK/TRSurveyView.h>
+#import <TheoremReachSDK/TRSurveyViewController.h>
+#import <TheoremReachSDK/TRUtils.h>
+
